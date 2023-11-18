3be6628788d45fc96c82ca95663dd3918c89b9ab
siewdass <siewdass@gmail.com>
2023-11-18 14:28:20 -0400

    x

--- src/mongodb.service.ts
+++ src/mongodb.service.ts
@@ -10,3 +10,56 @@ export class MongodbService {
 	}
 
 }
+
+import { } from 'riser'
+
+//@Observable( )
+export class DataObservable {
+
+	name: string = 'sam'
+	email: string = 'sam@gmail.com'
+
+	//@Observable( )
+	myprop: string = ''
+	// properti lis
+	// JSON.stringify( { value: undefined } )
+
+	subscriptions: any = { }
+
+	publish( key: string, value: any ): void {
+		localStorage.setItem( `${this.constructor.name}-${key}`, JSON.stringify( value ) )
+		if ( this.subscriptions.hasOwnProperty( key ) ) this.subscriptions[ key ]( value )
+	}
+
+	subscribe( key: string, callback: Function ): void {
+		const value = localStorage.getItem( `${this.constructor.name}-${key}` )
+		if ( value ) callback( JSON.parse( value ) )
+		this.subscriptions[ key ] = callback
+	}
+
+	// PUEDO ENVIAR THIS en el constructor del component o mandar el id para el storages
+
+	clear( ) {
+	}
+
+}
+
+new class {
+
+	//@Observable( 'data' ) // aqui se el id de la clase, el nombre de la propiedad quizas se pueda llamar global
+	myprop: string = ''
+
+	other: string = '123'
+	data: DataObservable = new DataObservable
+
+	constructor( ) {
+		this.data.subscribe( 'ss', v => this.other = v )
+		this.data.publish( 'ss', '321' )
+		//this.data.email.publish( 'ss', '321' )
+
+		setTimeout( ( ) => {
+			console.log('asdasdasdasdasdasdasdasd',this.other)
+		}, 2000 )
+	}
+
+}
--- src/user/user.component.tsx
+++ src/user/user.component.tsx
@@ -1,5 +1,6 @@
 import { Component, Publish, Navigate, Subscribe, State, Client } from 'riser'
 import { Button, Input } from 'riser/interface'
+
 @Component( )
 export class UserComponent {
 
@@ -11,9 +12,6 @@ export class UserComponent {
 	@State( )
 	users: string[] = []
 
-	@State( )
-	pass: boolean = false
-
 	onRegister( ) {
 		if ( this.input == '' ) return
 		Publish( '/user/create', this.input )
@@ -23,7 +21,6 @@ export class UserComponent {
 
 	@Subscribe( '/user/read' )
 	getList( users: any ) {
-		this.pass = true
 		this.users = users
 	}
 
@@ -34,7 +31,7 @@ export class UserComponent {
 					<Input placeholder={ 'Write your name!' } value={ this.input }/>
 					<Button label={ 'Register' } onClick={ this.onRegister }/>
 				</div>
-				{ this.pass == false ? <></> :
+				{ this.users.length == 0 ? <></> :
 				<ul class="m-4 mt-0 list-none list-inside text-blue-dark border w-[363px] h-[220px] overflow-auto rounded">
 					{ this.users.map( ( u: any ) => <li class="h-[33px] border" onClick={ () => { Client( this.user ); Navigate( `/message?from=${ this.user }&to=${ u.name }` ) } }>{ u.name }</li> ) }
 				</ul> }
